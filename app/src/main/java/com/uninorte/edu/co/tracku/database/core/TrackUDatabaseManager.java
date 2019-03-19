4ce14265710337dd5b09028243cde4101c523e1c
Raul H Juliao Colina <raulj@uninorte.edu.co>
2019-03-18 23:01:26 -0500

    Completada la vista del histórico de posiciones. Se añadió la nueva tabla para verificar si un usuario esta conectado a la base de datos y cual fue su última ubicación.

--- app/src/main/java/com/uninorte/edu/co/tracku/database/core/TrackUDatabaseManager.java
+++ app/src/main/java/com/uninorte/edu/co/tracku/database/core/TrackUDatabaseManager.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import com.uninorte.edu.co.tracku.database.daos.GpsDao;
+import com.uninorte.edu.co.tracku.database.daos.LastLocationDao;
 import com.uninorte.edu.co.tracku.database.daos.UserDao;
 import com.uninorte.edu.co.tracku.database.entities.GPSlocation;
 import com.uninorte.edu.co.tracku.database.entities.LastLocation;
@@ -14,4 +15,6 @@ public abstract class TrackUDatabaseManager extends RoomDatabase {
     public abstract UserDao userDao();
 
     public abstract GpsDao locationDao();
+
+    public abstract LastLocationDao lastLocationDao();
 }
