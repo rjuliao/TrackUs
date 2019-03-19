95a35cbde99f8518008b4ec8d1b7a8da0bd6200d
Raul H Juliao Colina <raulj@uninorte.edu.co>
2019-03-18 21:09:59 -0500

    Algo de web service

--- app/src/main/java/com/uninorte/edu/co/tracku/RegistrationActivity.java
+++ app/src/main/java/com/uninorte/edu/co/tracku/RegistrationActivity.java
@@ -7,21 +7,32 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.EditText;
 
-public class RegistrationActivity extends Activity implements View.OnClickListener {
+import com.uninorte.edu.co.tracku.networking.WebServiceManager;
+import com.uninorte.edu.co.tracku.networking.WebServiceManagerInterface;
 
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RegistrationActivity extends Activity implements View.OnClickListener, WebServiceManagerInterface {
+    String fname = "";
+    String lname = "";
+    String userName="";
+    String password="";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_registration);
-        
         findViewById(R.id.reg_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentToBeCalled=new Intent();
-                String fname = ((EditText)findViewById(R.id.reg_fname_value)).getText()+"";
-                String lname = ((EditText)findViewById(R.id.reg_lname_value)).getText()+"";
-                String userName=((EditText)findViewById(R.id.reg_username_value)).getText()+"";
-                String password=((EditText)findViewById(R.id.reg_password_value)).getText()+"";
+                fname = ((EditText)findViewById(R.id.reg_fname_value)).getText()+"";
+                lname = ((EditText)findViewById(R.id.reg_lname_value)).getText()+"";
+                userName=((EditText)findViewById(R.id.reg_username_value)).getText()+"";
+                password=((EditText)findViewById(R.id.reg_password_value)).getText()+"";
                 String passwordConfirmation=((EditText)findViewById(R.id.reg_password_confirmation_value)).getText()+"";
 
                 if(password.equals(passwordConfirmation)) {
@@ -33,6 +44,7 @@ public class RegistrationActivity extends Activity implements View.OnClickListen
                     intentToBeCalled.setClass(getApplicationContext(), MainMenuAct.class);
                     startActivity(intentToBeCalled);
                 }
+
             }
         });
 
@@ -55,6 +67,48 @@ public class RegistrationActivity extends Activity implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
+
+    }
+    public void registrarWS(){
+        try{
+            WebServiceManager.CallWebServiceOperation(this, "http","POST","Payload", "Estado");
+            try{
+                URL url=new URL("");
+                String methodType= "POST";
+                String datos= fname+","+lname+","+userName+","+password;
+                HttpURLConnection httpURLConnection= (HttpURLConnection)url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setRequestMethod(methodType);
+                httpURLConnection.getOutputStream();
+                BufferedWriter w= new BufferedWriter(new OutputStreamWriter(httpURLConnection.getOutputStream()));
+                w.write(datos);w.flush();w.close();
+                int responseCode=httpURLConnection.getResponseCode();
+                if(responseCode==HttpURLConnection.HTTP_OK){
+                    InputStream in=httpURLConnection.getInputStream();
+                    StringBuffer stringBuffer=new StringBuffer();
+                    int charIn=0;
+                    while((charIn=in.read())!=-1){
+                        stringBuffer.append((char)charIn);
+                    }
+                    String line= stringBuffer.toString();
+                }else{
+                    //local :v
+                }
+
+            }catch (Exception error){
+
+            }
+        }catch (Exception e){
+
+        }
+    }
+
+
+
+
+    @Override
+    public void WebServiceMessageReceived(String userState, String message) {
 
     }
 }
